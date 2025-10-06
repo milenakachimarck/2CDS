245106b7a2931fe2fd278a2168d671fb459262dd
milenak <milena.kachimarck@escola.pr.gov.br>
2025-10-06 17:47:53 -0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Exemplo 1 — mostrar uma mensagem
+
 function mostrarMensagem() {
   alert("Olá! Seja bem-vindo(a) ao site Nossa escola");
 }
 
-// Exemplo 2 — calcular média de notas
 function calcularMedia() {
   const nota1 = parseFloat(prompt("Digite a primeira nota:"));
   const nota2 = parseFloat(prompt("Digite a segunda nota:"));
@@ -18,6 +17,7 @@ function calcularMedia() {
   if (media >= 7) {
     alert(`Aprovado! Sua média foi ${media}`);
   } else {
+    if (media <= 7) {
     alert(`Reprovado! Sua média foi ${media}`);
   }
 }
