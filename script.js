39b025984fb648ef87707acfcac3dde72aea1a91
milenak <milena.kachimarck@escola.pr.gov.br>
2025-10-13 15:04:31 -0300

    .

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const imagem = document.querySelectorAll('img');
+document.addEventListener('DOMContentLoaded', () => {
+  const imagens = document.querySelectorAll('img');
 
-imagem.forEach(img => {
-  img.addEventListener('mouseenter', () => {
+  imagens.forEach(img => {
+    img.addEventListener('mouseenter', () => {
       img.style.transform = 'translateY(-10px)';
       img.style.boxShadow = '0 10px 20px rgba(0, 0, 0, 0.2)';
-  });
+    });
 
-  img.addEventListener('mouseleave', () => {
+    img.addEventListener('mouseleave', () => {
       img.style.transform = 'translateY(0)';
       img.style.boxShadow = 'none';
+    });
   });
 });
