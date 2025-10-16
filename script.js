b2e7355142e39d89df5a707f9e348cbf11f8715c
milenak <milena.kachimarck@escola.pr.gov.br>
2025-10-16 11:21:44 -0300

    .

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const imagens = document.querySelectorAll('img');
+    const botaoTema = document.getElementById("tema");
 
-  imagens.forEach(img => {
-    img.addEventListener('mouseenter', () => {
-      img.style.transform = 'translateY(-10px)';
-      img.style.boxShadow = '0 10px 20px rgba(0, 0, 0, 0.2)';
-    });
+    if (botaoTema) {
+        botaoTema.addEventListener("click", () => {
+            document.body.classList.toggle("dark");
+            
+            if (document.body.classList.contains("dark")) {
+                botaoTema.textContent = '☀️ Modo claro';
+            } else {
+                botaoTema.textContent = '🌙 Modo escuro';
+            }
+        });
+    }
+   
+    const imagens = document.querySelectorAll('img');
 
-    img.addEventListener('mouseleave', () => {
-      img.style.transform = 'translateY(0)';
-      img.style.boxShadow = 'none';
+    imagens.forEach(img => {
+        img.addEventListener('mouseenter', () => {
+            img.style.transform = 'translateY(-10px)';
+            img.style.boxShadow = '0 10px 20px rgba(0, 0, 0, 0.2)';
+        });
+
+        img.addEventListener('mouseleave', () => {
+           
+            img.style.transform = 'translateY(0)';
+            img.style.boxShadow = 'none'; 
+        });
     });
-  });
 });
+
 window.onload = function() {
-  alert("Bem-vindo(a) ao site da Nossa Escola!");
+    alert("Bem-vindo(a) ao site da Nossa Escola!");
 };
-const botaoTema = document.getElementById("tema");
-
-botaoTema.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-});
